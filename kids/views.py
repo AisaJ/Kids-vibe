11b7d636cc4ad49a7b03cdf3a193ff02f74cf4ac
Aisa <aisajemila@gmail.com>
2019-06-05 21:01:56 +0300

    Added content to the base template and navbar. Set landing page header.

--- kids/views.py
+++ kids/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest,Http404
 # Create your views here.
+
+def home(request):
+  title='Kids-Vibe'
+  return render (request,'home.html',{"title":title})
