7f64cffbe3584bb7007961cd4cacc960e0015d24
Aisa <aisajemila@gmail.com>
2019-06-06 15:30:32 +0300

    Registered the models on the admin site.

--- kids/admin.py
+++ kids/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import KidsClub,KidsCorner,Profile
 # Register your models here.
+
+admin.site.register(KidsClub)
+admin.site.register(KidsCorner)
+admin.site.register(Profile)
