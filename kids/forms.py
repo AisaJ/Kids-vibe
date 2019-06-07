629caafd2c95e0d6043184061722ea9f8a0fc7d6
Aisa <aisajemila@gmail.com>
2019-06-07 13:02:12 +0300

    Linked the join club button to profile creation to join the club.

--- kids/forms.py
+++ kids/forms.py
@@ -5,3 +5,12 @@ class NewProfileForm(forms.ModelForm):
   class Meta:
     model=Profile
     exclude=['user']
+
+class NewCornerForm(forms.ModelForm):
+  class Meta:
+    model=KidsCorner
+    fields='__all__'
+
+class NewClubForm(forms.ModelForm):
+  class Meta:
+    model=
