51dfba51721e0bf7c5b210a4e97aad4150bcc84a
Aisa <aisajemila@gmail.com>
2019-06-07 04:49:21 +0300

    added a drop down menu list.

--- kids/views.py
+++ kids/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest,Http404
 from .models import KidsClub,KidsCorner,Profile
 from django.contrib.auth.decorators import login_required
+from .forms import NewProfileForm
 # Create your views here.
 
 def home(request):
